a542a34144e2a16868f7102254fc64dfe5d728c3
ChadKreutzer <kanthalion@gmail.com>
2017-01-23 21:45:21 -0700

    database migration and .gitignore

---
+++ db/migrate/20170124043928_create_projects.rb
@@ -0,0 +1,10 @@
+class CreateProjects < ActiveRecord::Migration[5.0]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.string :image_link
+      t.string :url
+      t.string :description
+    end
+  end
+end
