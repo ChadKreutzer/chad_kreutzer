d49721854066233fc9a9fe1e990a810e3925b1e1
Chad Kreutzer <kanthalion@gmail.com>
2017-01-25 08:33:22 +0000

    setup sendgrid

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -16,17 +16,15 @@ get '/' do
                 body: "#{name}\n#{mail}\n\n#{body}",
                 via: :smtp,
                 via_options: {
-                  address:              'smtp.gmail.com',
+                  address:              'smtp.sendgrid.net',
                   port:                 '587',
                   enable_starttls_auto: true,
-                  user_name:            ENV["GMAIL_USERNAME"],
-                  password:             ENV["GMAIL_PASSWORD"],
+                  user_name:            ENV['SENDGRID_USERNAME'],
+                  password:             ENV['SENDGRID_PASSWORD'],
                   authentication:       :plain, # :plain, :login, :cram_md5, no auth by default
                   domain:               "localhost.localdomain" # the HELO domain provided by the client to the server
                 }
               })
-  
-   # Look into figaro gem for .gitignore.
   
     redirect '/#contact'
   end
