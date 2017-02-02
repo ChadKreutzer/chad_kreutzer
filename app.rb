5f710bddbb23a29488f528b5772d0fe4f44876cb
ChadKreutzer <kanthalion@gmail.com>
2017-02-02 16:38:34 -0700

    clean up app.rb in vim

--- app.rb
+++ app.rb
@@ -6,31 +6,31 @@ Sass::Plugin.options[:style] = :compressed
 use Sass::Plugin::Rack
 
 get '/' do
-    @projects = Project.all
-    erb :index
-  end
+  @projects = Project.all
+  erb :index
+end
 
-  post '/' do
-  
-    name  = params[:name]
-    mail = params[:mail]
-    subject = params[:subject]
-    body  = params[:body]
+post '/' do
+
+  name  = params[:name]
+  mail = params[:mail]
+  subject = params[:subject]
+  body  = params[:body]
     
-    Pony.mail({ to:'chadkreutzer@outlook.com',
-                subject: "#{subject}",
-                body: "#{name}\n#{mail}\n\n#{body}",
-                via: :smtp,
-                via_options: {
-                  address:              'smtp.sendgrid.net',
-                  port:                 '587',
-                  enable_starttls_auto: true,
-                  user_name:            ENV['SENDGRID_USERNAME'],
-                  password:             ENV['SENDGRID_PASSWORD'],
-                  authentication:       :plain, # :plain, :login, :cram_md5, no auth by default
-                  domain:               "localhost.localdomain" # the HELO domain provided by the client to the server
-                }
-              })
+  Pony.mail({ to:'chadkreutzer@outlook.com',
+              subject: "#{subject}",
+              body: "#{name}\n#{mail}\n\n#{body}",
+              via: :smtp,
+              via_options: {
+                address:              'smtp.sendgrid.net',
+                port:                 '587',
+                enable_starttls_auto: true,
+                user_name:            ENV['SENDGRID_USERNAME'],
+                password:             ENV['SENDGRID_PASSWORD'],
+                authentication:       :plain, # :plain, :login, :cram_md5, no auth by default
+                domain:               "localhost.localdomain" 
+              }
+            })
   
-    redirect '/#contact'
-  end
+  redirect '/#contact'
+end
