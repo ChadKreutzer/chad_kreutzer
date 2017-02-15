af61cc8f72c16d02c5f415f602018d2e8f1fb082
Chad Kreutzer <kanthalion@gmail.com>
2017-02-15 09:56:52 +0000

    enable rackup

--- app.rb
+++ app.rb
@@ -2,35 +2,37 @@ require 'bundler'
 Bundler.require
 Dir["./models/*"].each {|file| require file }
 
-Sass::Plugin.options[:style] = :compressed
-use Sass::Plugin::Rack
-
-get '/' do
-  @projects = Project.all
-  erb :index
-end
-
-post '/' do
-
-  name  = params[:name]
-  mail = params[:mail]
-  subject = params[:subject]
-  body  = params[:body]
-    
-  Pony.mail({ to:'chadkreutzer@outlook.com',
-              subject: "#{subject}",
-              body: "#{name}\n#{mail}\n\n#{body}",
-              via: :smtp,
-              via_options: {
-                address:              'smtp.sendgrid.net',
-                port:                 '587',
-                enable_starttls_auto: true,
-                user_name:            ENV['SENDGRID_USERNAME'],
-                password:             ENV['SENDGRID_PASSWORD'],
-                authentication:       :plain,
-                domain:               "localhost.localdomain" 
-              }
-            })
+class MyApp < Sinatra::Base
+  Sass::Plugin.options[:style] = :compressed
+  use Sass::Plugin::Rack
   
-  redirect '/#contact'
+  get '/' do
+    @projects = Project.all
+    erb :index
+  end
+  
+  post '/' do
+  
+    name  = params[:name]
+    mail = params[:mail]
+    subject = params[:subject]
+    body  = params[:body]
+      
+    Pony.mail({ to:'chadkreutzer@outlook.com',
+                subject: "#{subject}",
+                body: "#{name}\n#{mail}\n\n#{body}",
+                via: :smtp,
+                via_options: {
+                  address:              'smtp.sendgrid.net',
+                  port:                 '587',
+                  enable_starttls_auto: true,
+                  user_name:            ENV['SENDGRID_USERNAME'],
+                  password:             ENV['SENDGRID_PASSWORD'],
+                  authentication:       :plain,
+                  domain:               "localhost.localdomain" 
+                }
+              })
+    
+    redirect '/#contact'
+  end
 end
